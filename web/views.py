fcb390ee0efb7c2923b09cd772142bb60710c6cd
lkventina <lkventina@gmail.com>
2023-02-28 20:49:33 +0300

    Add registration

--- web/views.py
+++ web/views.py
@@ -1,6 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from web.forms import RegistrationForm
+
+User = get_user_model()
 
 
 def main_view(request):
     return render(request, "web/main.html")
+
+def registration_view(request):
+    form = RegistrationForm()
+    is_success = False
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+            )
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            is_success = True
+    return render(request, "web/registration.html", {
+        "form": form, "is_success": is_success
+    })
+
