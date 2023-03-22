93526445c55e286a19ea4e4bfd60f7b51a881555
lkventina <lkventina@gmail.com>
2023-03-22 17:55:12 +0300

    Add task creating form

--- web/views.py
+++ web/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from web.forms import RegistrationForm, AuthForm
+from web.forms import RegistrationForm, AuthForm, TaskForm
 
 User = get_user_model()
 
@@ -10,15 +10,16 @@ User = get_user_model()
 def main_view(request):
     return render(request, "web/main.html")
 
+
 def registration_view(request):
     form = RegistrationForm()
     is_success = False
     if request.method == 'POST':
-        form = RegistrationForm(data=request.POST)
+        form = RegistrationForm(data = request.POST)
         if form.is_valid():
             user = User(
-                username=form.cleaned_data['username'],
-                email=form.cleaned_data['email'],
+                username = form.cleaned_data['username'],
+                email = form.cleaned_data['email'],
             )
             user.set_password(form.cleaned_data['password'])
             user.save()
@@ -31,7 +32,7 @@ def registration_view(request):
 def auth_view(request):
     form = AuthForm()
     if request.method == 'POST':
-        form = AuthForm(data=request.POST)
+        form = AuthForm(data = request.POST)
         if form.is_valid():
             user = authenticate(**form.cleaned_data)
             if user is None:
@@ -41,6 +42,17 @@ def auth_view(request):
                 return redirect("main")
     return render(request, "web/auth.html", {"form": form})
 
+
 def logout_view(request):
     logout(request)
     return redirect("main")
+
+
+def task_add_view(request):
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(data=request.POST, initial={"user": request.user})
+        if form.is_valid():
+            form.save()
+            return redirect("main")
+    return render(request, "web/task_form.html", {"form": form})
