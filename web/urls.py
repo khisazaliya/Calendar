49b5ecf0efca1933822c4fa27bbe27d7ee66f2d6
lkventina <lkventina@gmail.com>
2023-03-22 22:18:17 +0300

    Add tags form

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from web.views import main_view, registration_view, auth_view, logout_view, task_edit_view
+from web.views import main_view, registration_view, auth_view, logout_view, task_edit_view, tags_view, tags_delete_view
 
 urlpatterns = [
     path("", main_view, name = "main"),
@@ -8,5 +8,7 @@ urlpatterns = [
     path("auth/", auth_view, name = "auth"),
     path("logout/", logout_view, name = "logout"),
     path("tasks/add/", task_edit_view, name = "tasks_add"),
-    path("tasks/<int:id>", task_edit_view, name = "tasks_edit")
+    path("tasks/<int:id>", task_edit_view, name = "tasks_edit"),
+    path("tags", tags_view, name = "tags"),
+    path("tags/<int:id>/delete/", tags_delete_view, name = "tags_delete"),
 ]
--- web/views.py
+++ web/views.py
@@ -2,8 +2,8 @@ from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from web.forms import RegistrationForm, AuthForm, TaskForm
-from web.models import Task
+from web.forms import RegistrationForm, AuthForm, TaskForm, TaskTagForm
+from web.models import Task, TaskTag
 
 User = get_user_model()
 
@@ -58,8 +58,25 @@ def task_edit_view(request, id=None):
         task = Task.objects.get(id=id)
     form = TaskForm(instance=task)
     if request.method == 'POST':
-        form = TaskForm(data=request.POST, initial={"user": request.user})
+        form = TaskForm(data=request.POST, files=request.FILES, initial={"user": request.user})
         if form.is_valid():
             form.save()
             return redirect("main")
     return render(request, "web/task_form.html", {"form": form})
+
+
+def tags_view(request):
+    tags = TaskTag.objects.all()
+    form = TaskTagForm()
+    if request.method == 'POST':
+        form = TaskTagForm(data = request.POST,  initial = {"user": request.user})
+        if form.is_valid():
+            form.save()
+            return redirect("tags")
+    return render(request, "web/tags.html", {"tags": tags, "form": form})
+
+
+def tags_delete_view(request, id):
+    tag = TaskTag.objects.get(id=id)
+    tag.delete()
+    return redirect('tags')
